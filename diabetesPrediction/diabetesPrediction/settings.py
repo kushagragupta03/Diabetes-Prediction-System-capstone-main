4bf6b40bf869e01b35c706a187765d15b9d64fc0
Kushagra Gupta <kushagra03gupta@gmail.com>
2025-03-30 03:19:43 +0530

    mm

--- diabetesPrediction/diabetesPrediction/settings.py
+++ diabetesPrediction/diabetesPrediction/settings.py
@@ -100,7 +100,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Include static folder
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Destination for collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

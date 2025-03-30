8ea1ad7dced898877be3b37e8d55d436f9fd7dba
Kushagra Gupta <kushagra03gupta@gmail.com>
2025-03-30 16:29:15 +0530

    mm

--- diabetesPrediction/diabetesPrediction/urls.py
+++ diabetesPrediction/diabetesPrediction/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-from .views import index
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('', views.index),
+    path('', views.index, name='index'),  # Removed duplicate
 ]
+
+# Add this to serve static files in development & Render
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
